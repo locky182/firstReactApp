4304d7dd7f3b7aef76604370dbca73d3907ff59d
locky182 <oven8209@gmail.com>
2022-11-28 18:12:02 +0300

    25 .map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
-let dialogsData =
+let dialogs =
     [{id: 0, name: 'Alex'},
         {id: 1, name: 'Petr'},
         {id: 2, name: 'Micha'},
         {id: 3, name: 'Pasha'},
         {id: 4, name: 'Masha'}
     ];
-let messagesData =
+
+let messages =
     [{id: 0, message:'Hi'},
         {id: 1, message: 'How are you'},
         {id: 2, message: 'How can i help you'},
         {id: 3, message: 'I dont now'},
         {id: 4, message: 'where are you from?'}
     ];
+
 
 
 const DialogItem = (props) => {
@@ -33,6 +35,13 @@ const Messages = (props) => {
     );
 }
 
+//Новые массивы, можно с ретурном
+let dialogsItems = dialogs.map(d => {return (<DialogItem name={d.name} id={d.id} />)})
+console.log(dialogsItems);
+//можно без ретурно сокращенно
+let messagesItem = messages.map(m => <Messages message={m.message} />)
+
+
 
 
 
@@ -41,21 +50,11 @@ const Dialogs = (props) => {
         <div className={s.dialogs}>
 
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-
-
+                { dialogsItems }
             </div>
-            <div className={s.messages}>
-                <Messages message={messagesData[0].message}/>
-                <Messages message={messagesData[1].message} />
-                <Messages message={messagesData[2].message} />
-                <Messages message={messagesData[3].message} />
-                <Messages message={messagesData[4].message} />
 
+            <div className={s.messages}>
+                { messagesItem }
             </div>
 
 
